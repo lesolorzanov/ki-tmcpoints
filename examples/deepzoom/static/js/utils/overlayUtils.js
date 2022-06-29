6eae7474a22a9e13968c5fbaa7b789fad6af8442
Leslie Solorzano <leslie.solorzano@ki.se>
2022-06-29 23:16:15 +0200

    removing region functionality

--- examples/deepzoom/static/js/utils/overlayUtils.js
+++ examples/deepzoom/static/js/utils/overlayUtils.js
@@ -69,21 +69,14 @@ overlayUtils.addTrackingToDOMElement= function(node,overlay) {
         dragHandler:function(event) {
             var viewportDelta=overlayUtils.viewportDelta(event.delta,overlay); 
             var d3node=d3.select(node);
+            console.log(node)
             var transformobj=overlayUtils.transformToObject(d3node.attr("transform"));
 
             transformobj.translate[0]=Number(transformobj.translate[0])+Number(viewportDelta.x);
             transformobj.translate[1]=Number(transformobj.translate[1])+Number(viewportDelta.y);
             //console.log(transformobj);
             d3node.attr("transform",overlayUtils.objectToTransform(transformobj));
-            //
-            if(!d3node.classed("regionp")){
-                var id=d3node.attr("id").split("-")[2];
-                var cellid="cell-"+overlay+"-"+id;
-                var cell=document.getElementById(cellid);
-                var OSDPoint=new OpenSeadragon.Point(transformobj.translate[0],transformobj.translate[1]);
-                var pToImageCoords=overlayUtils.pointToImage(OSDPoint,overlay);
-                cell.textContent= "("+Math.floor(pToImageCoords.x)+", "+ Math.floor(pToImageCoords.y)+")";
-            }
+            
         },
         dragEndHandler: function(event){
             var d3node=d3.select(node);
@@ -91,42 +84,7 @@ overlayUtils.addTrackingToDOMElement= function(node,overlay) {
             //perhaps do it only for region elements if possible
             //although this will imply differentiatin elemtns from regions ot nonreginos
             if(d3node.classed("regionp")){
-                //if a point from a region is dragged, erase the whole region and redraw
-                var id=d3node.attr("class").split("regionp-")[1].split(" ")[0];
-                var color=d3node.select("path").attr("stroke");
-                //console.log(id);
-                regionUtils.removePoly(id,overlay);
-                var regionid=overlay+id.toString();
-                var newpoints=[]//apend arrays of 2 values
-                d3.selectAll(".regionp-"+id+".TMCP-"+overlay)["_groups"][0].forEach(function(TMCP){
-                    var transform=overlayUtils.transformToObject(d3.select(TMCP).attr("transform"));                        
-                    var p0=Number(transform["translate"][0]);
-                    var p1=Number(transform["translate"][1]);
-                    newpoints.push([p0,p1]);                        
-                });
-                //var canvas=tmcpoints[overlay+"_svgov"].node();
-                //var drawingclass="regionpolygr regionpolygr-"+id+" "+overlay;
-                var regiongr=d3.select('.regionpolygr.'+regionid);
-                //var regiongr=d3.select("."+overlay+".regionpolygr-"+id);
-        
-                
-                //var reg=regionUtils.getRegion(id,overlay);
-                //var color=reg.color;
-                if(regionUtils._isNewRegion){
-                    regionUtils.modifyRegion(newpoints,id,overlay);
-
-                    regiongr.append('polygon').attr('points', newpoints)
-                        .style('fill', 'none').style("stroke", color)
-                        .attr('stroke-width', '0.0015')
-                        .attr('stroke', '#000').attr('class',"regionpoly regionpoly-"+id);
-                }else{
-                    regiongr.selectAll('polyline').remove();
-                    regiongr.append('polyline').attr('points', newpoints)
-                        .style('fill', 'none').style("stroke", "rgb(0,0,0)")
-                        .attr('stroke-width', '0.0015')
-                        .attr('stroke', '#000').attr('class',"regionpoly regionpoly-"+id);
-                }
-                //regionUtils._regionD3Groups[overlay].select('regionpoly-'+id).remove();
+              
             } else if(!d3node.classed("regionp")){
                 //if it is not region it's point
                 var htmlid=d3node.attr("id");
